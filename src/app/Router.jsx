265f4bc3b763041511fcd6e07990fe879685c46b
Luchi <luli79302@gmail.com>
2024-06-25 19:26:13 +0200

    Creación de challenges

--- src/app/Router.jsx
+++ src/app/Router.jsx
@@ -3,6 +3,7 @@ import Landing from '../pages/Landing'
 import Jugador from '../pages/Jugador'
 import Observador from '../pages/Observador'
 import Perfil from '../pages/Perfil'
+import Challenges from '../pages/Challenges'
 
 const Router = () => {
     return (
@@ -12,6 +13,7 @@ const Router = () => {
                 <Route path="/jugador" element={<Jugador />} />
                 <Route path="/observador" element={<Observador />} />
                 <Route path="/perfil" element={<Perfil />} />
+                <Route path="/challenges" element={<Challenges />} />
             </Routes>
         </BrowserRouter>
     )
