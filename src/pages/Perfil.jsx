871f3ce02adf4dc32c53454b57b9ee46ba75788d
Andrea <andreamania10@gmail.com>
2024-06-25 17:54:33 +0200

    perfil

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -1,10 +1,45 @@
+import { useState } from "react";
+import useUserContext from "../app/UserProvider"
+import PerfilStyle from "../app/Styles"
 import NavBar from "../components/NavBar"
+import Landing from "./Landing";
 
 const Perfil = () => {
+    const [user,] = useUserContext({});
+    const [nombre, setNombre] = useState();
+    const [email, setEmail] = useState();
+    const [image, setImage] = useState(null);
+
+    const handleFileChange = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setImage(reader.result);
+                reader.readAsDataURL(file);
+            }
+        }
+    }
     return (
         <div>Perfil
-
-
+            {!user ?
+                <Landing />
+                :
+                <PerfilStyle>
+                    <div className="entrance">
+                        <input type="file" accept="image/*" onChange={handleFileChange} />
+                        {image && (
+                            <div className="foto">
+                                <img src={image} alt="Selected" style={{ maxWidth: '100%', maxHeight: '300px' }} />
+                            </div>
+                        )}
+                    </div>
+                    <div className="information">
+                        <p>Nombre del usuario: {nombre} </p>
+                        <p>Email: {email} </p>
+                    </div>
+                </PerfilStyle>
+            }
             <NavBar />
         </div>
     )
