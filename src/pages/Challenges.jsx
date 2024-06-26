523857311d36e85c7aa2cffdd7f2db07b67e591b
Andrea <andreamania10@gmail.com>
2024-06-26 19:49:04 +0200

    apuntarse

--- src/pages/Challenges.jsx
+++ src/pages/Challenges.jsx
@@ -1,25 +1,35 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import desafios from '../components/desafios.json'
 import NavBar from '../components/NavBar';
 import { useUserContext } from '../app/UserProvider';
 import { Link } from 'react-router-dom';
 
 const Challenges = () => {
-    const [isPlayer,] = useState(true);
+    const [isPlayer, setIsPlayer] = useState();
     const [challenges] = useState(desafios);
-    const [isWatcher,] = useState(true);
     const user = useUserContext();
 
-    const handlePlayerAction = (challengeId) => {
-        console.log('Player action: ', user.id, challengeId);
-    };
+    useEffect(() => {
+        settingRol();
+    }, [user]);
 
 
+    const settingRol = () => {
+        if (user?.rol === 0) {
+            setIsPlayer(true);
+        } else {
+            setIsPlayer(false);
+        }
+    }
+
+    const challengeSave = () => {
+        //id del desafio para que busque cuál es, y el id del usuario con un put
+    }
     return (
         <>
             {
                 challenges.map((challenge) =>
-                    <div className='challengeItem' key={challenge.id}>
+                    <div key={challenge.id}>
                         <h2>{challenge.name}</h2>
                         <div className='challengeInfo'>
                             <p>{challenge.date}</p>
@@ -27,14 +37,16 @@ const Challenges = () => {
                         </div>
                         {
                             isPlayer &&
-                            <button onClick={() => handlePlayerAction(challenge.id)}>Apuntarse</button>
+                            <button onClick={() =>
+                                challengeSave()
+                            }>Apuntarse</button>
                         }
                     </div>
                 )
 
             }
             {
-                isWatcher &&
+                !isPlayer &&
                 <Link to={"/createChallenge"} />
             }
             <NavBar />
