4e4a01d23bed79bda7c28f5dad4e05e9b4b1177f
fernjgs <ferchito.285.fg@gmail.com>
2024-06-28 17:29:08 +0200

    subida

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+
 import { GlobalStyle } from './app/Styles';
 import UserProvider from './app/UserProvider';
 import Router from "./app/Router";
 
 const App = () => (
     <UserProvider>
-        <GlobalStyle/>
+        <GlobalStyle />
         <Router />
     </UserProvider>
 );
