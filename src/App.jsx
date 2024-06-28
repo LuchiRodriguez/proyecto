b9d624932789f8076b0d48982090076df4952af5
fernjgs <ferchito.285.fg@gmail.com>
2024-06-28 18:15:52 +0200

    subida

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,12 @@ import { GlobalStyle } from './app/Styles';
 import UserProvider from './app/UserProvider';
 import Router from "./app/Router";
 
-<<<<<<< HEAD
-const App = () => (
-    <UserProvider>
-        <GlobalStyle />
-        <Router />
-    </UserProvider>
-);
-=======
+
+
 const App = () => {
     const { user } = UserHook();
     return <>{user ? <Router /> : <PublicRoutes />}</>;
 };
->>>>>>> 1520c4af2394d6ebe4b269d7ecca37fb534758f4
+
 
 export default App;
