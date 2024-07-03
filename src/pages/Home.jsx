105808f59bc113163d818bf9591f7e22b7924b4b
Luchi <luli79302@gmail.com>
2024-07-03 19:16:12 +0200

    styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,16 +2,16 @@ import { useEffect, useState } from "react";
 // import ReactPlayer from "react-player";
 import NavBar from "../components/NavBar";
 import { getChallenges } from "../app/api/Challenge";
-import { UserInfo, ChallengeBox } from '../app/Styles';
+import { UserInfo, ChallengeBox } from "../app/Styles";
 
 const Home = () => {
   const [videos, setVideos] = useState([]);
 
-  const challenges = videos.filter(video => video.videoUrl !== null);
+  const challenges = videos.filter((video) => video.videoUrl !== null);
 
   useEffect(() => {
     getChallenges().then((data) => {
-      setVideos(data.data)
+      setVideos(data.data);
     });
   }, []);
 
@@ -20,12 +20,22 @@ const Home = () => {
       {challenges?.map((challenge) => (
         <ChallengeBox key={challenge.id}>
           <UserInfo>
-            {challenge.watcher.imagenUrl ? <img src={challenge.watcher.imagenUrl} /> : <img src="https://res.cloudinary.com/dappzkn6l/image/upload/v1719672139/21104_jqfpvo.png" alt="" />}
+            {challenge.watcher.imagenUrl ? (
+              <img src={challenge.watcher.imagenUrl} />
+            ) : (
+              <img
+                src="https://res.cloudinary.com/dappzkn6l/image/upload/v1719672139/21104_jqfpvo.png"
+                alt=""
+              />
+            )}
             <p>{challenge.player.username}</p>
           </UserInfo>
           <p>{challenge.description}</p>
-          <p className="player"> Challenged by <span>{challenge.watcher.username}</span></p>
-          <video src={challenge.videoUrl} autoPlay controls></video>
+          <p className="player">
+            {" "}
+            Challenged by <span>{challenge.watcher.username}</span>
+          </p>
+          <video src={challenge.videoUrl} autoPlay controls loop></video>
         </ChallengeBox>
       ))}
       <NavBar />
