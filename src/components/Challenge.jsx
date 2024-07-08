a2b3cde3f44eef787e6fb66fd54dbbb5d23ffa96
Luchi <luli79302@gmail.com>
2024-07-08 17:17:10 +0200

    .

--- src/components/Challenge.jsx
+++ src/components/Challenge.jsx
@@ -19,7 +19,7 @@ const Challenge = ({ ch, refetch }) => {
   const [acceptChallengeError, setAcceptChallengeError] = useState("");
 
   const navigate = useNavigate();
-  console.log(ch.watcher.username, 1111111111)
+  console.log(ch.watcher.username, 1111111111);
   const handleVideo = async (e) => {
     e.preventDefault();
     setIsUploading(true);
@@ -27,7 +27,7 @@ const Challenge = ({ ch, refetch }) => {
 
     const formData = new FormData();
     formData.append("file", file);
-    console.log(file)
+    console.log(file);
     formData.append("player", user.username);
     formData.append("watcher", ch.watcher.username);
     formData.append("points", ch.points);
@@ -75,48 +75,49 @@ const Challenge = ({ ch, refetch }) => {
         )}
         <p>{ch.watcher.username}</p>
       </UserInfo>
-      <p>Challenges you to: {ch.description}</p>
-      <p>Reward: {ch.points}</p>
+      <ChallengeInfo>
+        <p>Challenges you to: {ch.description}</p>
+        <p>Reward: {ch.points}</p>
 
-      {ch.player != null ? (
-        <p className="watcher">
-          Accepted by <span>{ch.player.username}</span>
-        </p>
-      ) : (
-        user.rol == "player" && (
-          <>
-            <ButtonStyle onClick={() => handleClick()}>
-              Accept challenge
-            </ButtonStyle>
-            {acceptChallengeError && (
-              <p style={{ color: "red" }}>{acceptChallengeError}</p>
+        {ch.player != null ? (
+          <p className="watcher">
+            Accepted by <span>{ch.player.username}</span>
+          </p>
+        ) : (
+          user.rol == "player" && (
+            <>
+              <ButtonStyle onClick={() => handleClick()}>
+                Accept challenge
+              </ButtonStyle>
+              {acceptChallengeError && (
+                <p style={{ color: "red" }}>{acceptChallengeError}</p>
+              )}
+            </>
+          )
+        )}
+
+        {ch.player != null && (
+          <UploadVideo>
+            {isUploading ? (
+              <UploadingDiv>
+                <img src="https://i.gifer.com/ZKZg.gif" alt="Uploading..." />
+                <h3>Uploading file, please wait...</h3>
+              </UploadingDiv>
+            ) : (
+              <form onSubmit={handleVideo} encType="multipart/form-data">
+                <input
+                  id="file-upload"
+                  type="file"
+                  accept="video/*"
+                  onChange={(e) => setFile(e.target.files[0])}
+                />
+              </form>
             )}
-          </>
-        )
-      )}
-
-      {ch.player != null && (
-        <UploadVideo>
-          {isUploading ? (
-            <UploadingDiv>
-              <img src="https://i.gifer.com/ZKZg.gif" alt="Uploading..." />
-              <h3>Uploading file, please wait...</h3>
-            </UploadingDiv>
-          ) : (
-            <form onSubmit={handleVideo} encType="multipart/form-data">
-              <input
-                id="file-upload"
-                type="file"
-                accept="video/*"
-                onChange={(e) => setFile(e.target.files[0])}
-              />
-            </form>
-          )}
-          {uploadError && <p style={{ color: "red" }}>{uploadError}</p>}
-        </UploadVideo>
-      )}
-    </ChallengeInfo>
-    </ChallengeBox >
+            {uploadError && <p style={{ color: "red" }}>{uploadError}</p>}
+          </UploadVideo>
+        )}
+      </ChallengeInfo>
+    </ChallengeBox>
   );
 };
 
