b9d65421ca2a10b1f5b5339b83bf1513d091fe89
Luchi <luli79302@gmail.com>
2024-07-08 14:19:04 +0200

    New landing page

--- src/pages/NewLanding.jsx
+++ src/pages/NewLanding.jsx
@@ -1,11 +1,95 @@
 import "../app/Landing.css";
 
 const NewLanding = () => {
+  const overallLoadTime = 7000;
+  const numberOfEls = 50;
+
+  /*
+   * Dash Array function
+   */
+  function createDashArray() {
+    const dashPattern = document.querySelectorAll(".dash-pattern");
+
+    function createDashes(i, element) {
+      let dash = document.createElement("div");
+      const positionY = (100 / numberOfEls) * i;
+      dash.classList.add("dash");
+      dash.style.left = positionY + "%";
+      dash.style.animationDelay = -1 + (1 / numberOfEls) * i + "s";
+      element.appendChild(dash);
+    }
+
+    dashPattern.forEach(function (element) {
+      for (let i = 0; i < numberOfEls; i++) createDashes(i, element);
+    });
+
+    return;
+  }
+
+  /*
+   * Helix function
+   */
+  function createHelix() {
+    function createStrand(i) {
+      let strand = document.createElement("div");
+      const rotate = (540 / numberOfEls) * i; //
+      const opacity = (1 / numberOfEls) * i;
+      const translateY = (100 / numberOfEls) * i - 50;
+      strand.classList.add("strand");
+      strand.style.opacity = opacity;
+      strand.style.transform = "rotate(" + rotate + "deg)";
+      strand.style.top = translateY + "%";
+    }
+
+    for (let i = 0; i < numberOfEls; i++) createStrand(i);
+
+    return;
+  }
+
+  /*
+   * Perect loading function
+   */
+  function percentLoader(id, start, end, duration) {
+    var range = end - start;
+    var current = start;
+    var increment = end > start ? 1 : -1;
+    var stepTime = Math.abs(Math.floor(duration / range));
+    var obj = document.getElementById(id);
+    var timer = setInterval(function () {
+      current += increment;
+      obj.innerHTML = current + "%";
+      if (current == end) {
+        clearInterval(timer);
+      }
+    }, stepTime);
+
+    return;
+  }
+
+  //speedUpCircles();
+
+  // Create Helix animation
+  createHelix();
+  // Create Dash array animations
+  createDashArray();
+  // Start loading text animation after setTimeout
+  setTimeout(function () {
+    percentLoader("loading", 0, 100, overallLoadTime - 4000);
+  }, overallLoadTime - 3000);
+
+  /* 
+ End the boot cycle
+*/
+
+  function pauseBoot() {
+    var newTimerId = window.setInterval("function(){}");
+    for (var i = 0; i <= newTimerId; i++) {
+      window.clearInterval(i);
+    }
+  }
+
   return (
     <>
-      <div className="text">
-        <h1>PRESS START</h1>
-      </div>
       <div className="app-container">
         <div className="overlay">
           <div className="scanlines"></div>
@@ -19,6 +103,10 @@ const NewLanding = () => {
       </div>
       <div className="boot-screen">
         <div className="boot-sequence">
+          <h1 className="boot-sequence-text">WELCOME</h1>
+          <button className="btn" id="js-fast-boot" onClick={() => pauseBoot()}>
+            PRESS START
+          </button>
           <div className="outter-circle">
             <svg
               className="white-circle"
@@ -46,7 +134,6 @@ const NewLanding = () => {
                 strokeDasharray="80 160"
               />
             </svg>
-
             <svg
               className="green-circle"
               viewBox="0 0 708 702"
@@ -61,6 +148,7 @@ const NewLanding = () => {
             </svg>
           </div>
         </div>
+        <div id="console" className="console"></div>
       </div>
     </>
   );
