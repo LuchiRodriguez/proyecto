f8186b5791fd3e7368a5fffbbc91ad7c2e1e58dd
DiegoV97 <velazquezdiego1502@gmail.com>
2024-07-17 18:06:59 +0200

    cambios challenge

--- src/components/ChallengeWithVideo.jsx
+++ src/components/ChallengeWithVideo.jsx
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-import { Suspense, useState, lazy, useRef, useEffect } from 'react';
-import ButtonLike from './ButtonLike';
-import NewComment from './NewComment';
-import { UserInfo, ChallengeInfo, ChallengeVideo, Interaction } from '../app/Styles';
-import PlayerComment from '../app/img/playerNavBar/playerDiscomment.png';
-import WatcherComment from '../app/img/watcherNavBar/watcherDiscommet.png';
-import { useUserContext } from '../app/UserProvider';
-
-const LazyVideo = lazy(() => import("../components/Lazyvideo"));
-
-const ChallengeWithVideo = ({ challenge, index, refetch, }) => {
-=======
 import { Suspense, useState, lazy, useRef, useEffect } from "react";
 import ButtonLike from "./ButtonLike";
 import NewComment from "./NewComment";
@@ -27,7 +14,6 @@ import { useUserContext } from "../app/UserProvider";
 const LazyVideo = lazy(() => import("../components/Lazyvideo"));
 
 const ChallengeWithVideo = ({ challenge, index, refetch }) => {
->>>>>>> master
   const [user] = useUserContext();
   const [showComments, setShowComments] = useState(false);
   const videoRefs = useRef([]);
@@ -45,25 +31,6 @@ const ChallengeWithVideo = ({ challenge, index, refetch }) => {
       },
       { threshold: 0.5 } // Adjust as needed
     );
-<<<<<<< HEAD
-=======
-
-    videoRefs.current.forEach((video) => {
-      if (video) {
-        observer.observe(video);
-      }
-    });
-
-    return () => {
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-      videoRefs.current.forEach((video) => {
-        if (video) {
-          observer.unobserve(video);
-        }
-      });
-    };
-  }, [challenge]);
->>>>>>> master
 
     videoRefs.current.forEach((video) => {
       if (video) {
@@ -86,29 +53,18 @@ const ChallengeWithVideo = ({ challenge, index, refetch }) => {
     <>
       <ChallengeVideo key={challenge.id}>
         <UserInfo>
-<<<<<<< HEAD
-          {challenge.player.imagenUrl !== null ?
-            <img src={challenge.player.imagenUrl} />
-            :
-=======
           {challenge.player.imagenUrl == null ? (
             <img src={challenge.player.imagenUrl} />
           ) : (
->>>>>>> master
             <img
               src="https://res.cloudinary.com/dappzkn6l/image/upload/v1719672139/21104_jqfpvo.png"
               alt=""
             />
-<<<<<<< HEAD
-          }
-          <p>{challenge.player.username}</p>
-=======
           )}
           <div>
             <p>{challenge.player.username}</p>
             <p>{challenge.transcurredTime}</p>
           </div>
->>>>>>> master
         </UserInfo>
         <ChallengeInfo>
           <p>{challenge.description}</p>
@@ -124,11 +80,7 @@ const ChallengeWithVideo = ({ challenge, index, refetch }) => {
           />
         </Suspense>
         <Interaction>
-<<<<<<< HEAD
-          <ButtonLike videoId={challenge.videos.id} />
-=======
           <ButtonLike />
->>>>>>> master
           <button onClick={() => setShowComments(!showComments)}>
             {user.rol === "watcher" ? (
               <img src={WatcherComment} alt="" />
@@ -137,9 +89,6 @@ const ChallengeWithVideo = ({ challenge, index, refetch }) => {
             )}
           </button>
         </Interaction>
-<<<<<<< HEAD
-        <NewComment comments={challenge.videos.comments} setShowComments={setShowComments} showComments={showComments} videoChallenge={challenge.videos.id} refetch={refetch} />
-=======
         <NewComment
           comments={challenge.videos.comments}
           setShowComments={setShowComments}
@@ -147,7 +96,6 @@ const ChallengeWithVideo = ({ challenge, index, refetch }) => {
           videoChallenge={challenge.videos.id}
           refetch={refetch}
         />
->>>>>>> master
       </ChallengeVideo>
     </>
   );
