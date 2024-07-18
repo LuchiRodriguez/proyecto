6751429557d94fc3e5f436f8830ad99969447844
DiegoV97 <velazquezdiego1502@gmail.com>
2024-07-18 18:04:38 +0200

    cambios del master

--- src/app/api/Video.js
+++ src/app/api/Video.js
@@ -1,12 +1,8 @@
 import { instance } from "./api";
 
-<<<<<<< HEAD
-export const postLike = async (obj) => await instance.post("/like", obj);
-=======
-export const postVideo = async (obj) =>{
+export const postVideo = async (obj) => {
   const data = await instance.post("/like", obj)
   console.log(data)
-} 
->>>>>>> master
+}
 
 export const getVideoById = async (id) => await instance.get(`/video/${id}`);
