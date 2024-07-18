2e61c71eb42d223892aab0ad0352cab0ed812bc2
Maxi <maaxiliria@gmail.com>
2024-07-18 15:33:56 +0200

    update likes

--- src/app/api/Video.js
+++ src/app/api/Video.js
@@ -1,13 +1,8 @@
 import { instance } from "./api";
 
 export const postVideo = async (obj) =>{
-  console.log(obj)
-  
   const data = await instance.post("/like", obj)
   console.log(data)
 } 
 
-
-
-export const postComments = async (obj) =>
-  await instance.post("/comments", obj);
+export const getVideoById = async (id) => await instance.get(`/video/${id}`);
--- src/components/ButtonLike.jsx
+++ src/components/ButtonLike.jsx
@@ -3,12 +3,37 @@ import likeImgWatcher from "../app/img/watcherNavBar/like.png";
 import likeImgPlayer from "../app/img/playerNavBar/like.png";
 import dislikePlayer from "../app/img/playerNavBar/dislike.png";
 import { useUserContext } from "../app/UserProvider";
-import { useState } from "react";
-import { postVideo } from "../app/api/Video";
+import { useEffect, useState } from "react";
+import {postVideo, getVideoById} from '../app/api/Video';
+import {getUserByUsername} from '../app/api/User';
 
 const ButtonLike = ({videoId, refetch}) => {
   const [user] = useUserContext();
   const [like, setLike] = useState(false);
+  const [userProfile, setUserProfile] = useState([]);
+  const [meGusta, setMeGusta] = useState([]);
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const userProfileData = await getUserByUsername(user.username);
+        setUserProfile(userProfileData);
+  
+        const videoData = await getVideoById(videoId);
+        setMeGusta(videoData.data.meGustas);
+  
+        if (userProfileData.meGustas.includes(videoId)) {
+          setLike(true);
+        } else {
+          setLike(false);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+  
+    fetchData();
+  }, [user.username, videoId]);
 
   const handleLike = async (e) => {
     e.preventDefault();
