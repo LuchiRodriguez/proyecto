76733fe4bc86b9b9061716b154b6e143f1ca4305
Luchi <luli79302@gmail.com>
2024-07-24 15:19:44 +0200

    Public routes

--- src/app/PublicRoutes.jsx
+++ src/app/PublicRoutes.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "../pages/Login";
 import NewLanding from "../pages/NewLanding";
+import VisitChallenge from "../pages/VisitChallenge";
 
 const PublicRoutes = () => {
   return (
@@ -8,6 +9,7 @@ const PublicRoutes = () => {
       <Routes>
         <Route index element={<NewLanding />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/visit/:id" element={<VisitChallenge />} />
       </Routes>
     </BrowserRouter>
   );
